62581db7d62df8cbd90fe11fb9d5c6d0e2f5f07e
Nur Mohsin <follow.mohsin@gmail.com>
2018-08-10 12:51:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,43 @@
-#Python libraries that we need to import for our bot
-import random
 from flask import Flask, request
-from pymessenger.bot import Bot
-import os 
+import requests
+
 app = Flask(__name__)
-ACCESS_TOKEN = "EAADVw0N2kHEBAGsaysJSKBGrS0SXAK91JaU1yDrShLaOywRoJshD3ZBYkDY2Jb9lPxCG2TRX5ZBwd4m2wwPb8DOKW7uU7dz1NT410b1QP1UK5exVVKP26Ldyo4izFp7lcXT4bcsPK18yDYLf5AkxxY1J2AF8uWHbpQ7Yvb0AZDZD" 
-#ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = 'VERIFY_TOKEN'   
-#VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
-bot = Bot (ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint 
-@app.route("/", methods=['POST','GET'])
-def receive_message():
-    if request.method == 'POST':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
-        token_sent = request.args.get("hub.verify_token")
-        # return verify_fb_token(token_sent)
-        return token_sent
-    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+ACCESS_TOKEN = "EAADVw0N2kHEBAGsaysJSKBGrS0SXAK91JaU1yDrShLaOywRoJshD3ZBYkDY2Jb9lPxCG2TRX5ZBwd4m2wwPb8DOKW7uU7dz1NT410b1QP1UK5exVVKP26Ldyo4izFp7lcXT4bcsPK18yDYLf5AkxxY1J2AF8uWHbpQ7Yvb0AZDZD"
+
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    unknown = "Unknown text! I will reply you later. :)"
+    hlw = "Hello"
+    sm_emo = ":)"
+    if message == "hi":
+        reply(sender, hlw)
+    elif message == "how are you?":
+        reply(sender, "fine. you?")
+    elif message == "ok":
+        reply(sender, sm_emo)
+    elif message == sm_emo:
+        reply(sender, sm_emo)
+    elif message == "hmm":
+        reply(sender, "hmm2 :D")
     else:
-        # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
-    return "Message Processed"
+        reply(sender, unknown)
 
+    return "ok"
 
-def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
-
-
-#chooses a random message to send to the user
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
-
-#uses PyMessenger to send response to user
-def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
-    bot.send_text_message(recipient_id, response)
-return "success"
 
 if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
