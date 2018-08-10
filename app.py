47b949632a112ed6121eba600c438a65e3211474
Nur Mohsin <follow.mohsin@gmail.com>
2018-08-10 12:40:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook.""" 
-        token_sent = request.args.get(hub.verify_token)
+        token_sent = request.args.get("hub.verify_token")
         # return verify_fb_token(token_sent)
         return token_sent
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
@@ -42,7 +42,7 @@ def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
     #if they match, allow the request, else return an error 
     if token_sent == VERIFY_TOKEN:
-        return request.args.get(hub.challenge)
+        return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
 
