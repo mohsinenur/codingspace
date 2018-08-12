b5bf471614d45438d89ef93c0534f2440f93515c
Nur Mohsin <follow.mohsin@gmail.com>
2018-08-12 10:43:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
-    return "Hello World!"
+    if methods=='GET':
+        return "Hello World!"
+    else:
+        return "Working Well!"
 
 
 if __name__ == '__main__':
