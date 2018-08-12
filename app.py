71c88f9a1287d52afe2d9f375b2ab68486f93134
Nur Mohsin <follow.mohsin@gmail.com>
2018-08-12 15:23:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 r_msg = message['message']['text']
                 if message['message'].get('text'):
-                    if r_msg == 'hi' or 'hello'
-                    response_sent_text = recipient_msg
+                    response_sent_text = get_text_message()
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
