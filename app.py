8af0b28a31a7bd7c2737a7ac1071150c42bf6d20
Nur Mohsin <follow.mohsin@gmail.com>
2018-08-15 15:12:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,19 @@ def temperature():
     return tempereature
 
 
+def sun_set():
+    w_api_address = 'https://api.darksky.net/forecast/66f19b250a5e0730c037c857603339f4/23.8103,90.4125'
+    data = requests.get(w_api_address).json()
+    weather = {
+        'sun_set': data['daily']['data']['sunsetTime'],
+        'sun_rise': data['daily']['data']['sunriseTime']
+    }
+    s_set = int(weather['sun_set'])
+    s_rise = int(weather['sun_rise'])
+    sun_value = 'Now Dhaka sunset at ' + str(s_set) + ' sunrise at ' + str(s_rise) 
+    return sun_value
+
+
 def text_matching():
     t_hi = ['hi','Hi','HI','hI']
     t_hello = ['hello','Hello','HELLO','hlw','Hlw','HLW']
@@ -154,6 +167,7 @@ def text_matching():
     t_who_r_u = ['who are you?', 'Who are you?']
     t_temp = ['temperature in dhaka', 'what is temperature in dhaka?', 'dhaka temperature', 'dhaka temp', 'temp']
     t_tnx = ['tnx', 'thanks', 'thank you', 'Thanks', 'Tnx', 'Thank you']
+    t_sun_set = ['sun set', 'sun rise', 'sun']
 
     # matching text for reply
     if r_msg in t_hi:
@@ -178,6 +192,8 @@ def text_matching():
         response_sent_text = 'I am Bot. Made by SSL Developer Team.'
     elif r_msg in t_temp:
         response_sent_text = temperature()
+    elif r_msg in t_sun_set:
+        response_sent_text = sun_set()
     else:
         response_sent_text = "Unknown text! You can type 'temp' to know temperature. :)"
     
